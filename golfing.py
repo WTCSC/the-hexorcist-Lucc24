3cbcf76ed5685df052f5a453dca8b4e25a4b6fac
Lucc24 <luccatownsend@icloud.com>
2025-11-19 14:25:53 -0700

    golf more

--- golfing.py
+++ golfing.py
@@ -1,22 +1,21 @@
-digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.upper()
-def to_decimal(number_string, original_base):
-    total_value = 0
-    for power, char in enumerate(number_string[::-1]):
-        total_value += digits.index(char) * (original_base ** power)
-    return total_value
-def from_decimal(decimal_number, target_base):
-    if decimal_number == 0:
-        return '0'
-    result_string = ''
-    while decimal_number > 0:
-        result_string = digits[decimal_number % target_base] + result_string
-        decimal_number //= target_base
-    return result_string
-if __name__ == "__main__":
-    asknumstring = input("Welcome to whatever this is maybe its a calculator idk \n Please enter the number you would like to convert: ").upper()
-    askoriginalbase = int(input("Please enter the numbers current base (2-36): "))
-    asktargetbase = int(input("Please enter the numbers target base (2-36): "))
-    converted_value = from_decimal(to_decimal(asknumstring, askoriginalbase), asktargetbase)
-    print(f"The number \"{asknumstring}\" is \"{converted_value}\" in base-{asktargetbase}.")
+d='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def q(n,o):
+    t=0
+    for p,c in enumerate(n[::-1]):
+        t += d.index(c)*(o**p)
+    return t
+def f(n,o):
+    if n==0:
+        return'0'
+    r=''
+    while n>0:
+        r=d[n%o]+r
+        n//=o
+    return r
+a=input("Welcome to whatever this is maybe its a calculator idk \n Please enter the number you would like to convert: ")
+s=int(input("Please enter the numbers current base (2-36): "))
+t=int(input("Please enter the numbers target base (2-36): "))
+z=f(q(a, s), t)
+print(f"The number \"{a}\" is \"{z}\" in base-{t}.")
 
 
